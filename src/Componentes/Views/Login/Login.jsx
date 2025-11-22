ad59c2d6a86de69c013b65f81bb2e90983aa0d07
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-11-22 02:56:54 -0300

    fix: Corrige, anterior commit con Login vacio. Ahora completo con estructura base.

--- src/Componentes/Views/Login/Login.jsx
+++ src/Componentes/Views/Login/Login.jsx
@@ -1,6 +1,27 @@
+export const Login = ({ onClose }) => {
+  const onSubmit = (data) => {
+    console.log("envio de datos login");
+    onClose();
+  };
 
-export const Login = ({onClose}) => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      {" "}
+      <Modal
+        show={true}
+        onHide={onClose}
+        centered
+        size="lg"
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton onClose={onClose}>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FormLogin onSubmit={onSubmit} />
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
